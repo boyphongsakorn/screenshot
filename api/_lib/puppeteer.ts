0b8d04778867f3c906984e3e7efe91e5d0a4264c
Phongsakorn Wisetthon <boy1556@hotmail.com>
2022-05-21 14:58:04 +0700

    test

--- api/_lib/puppeteer.ts
+++ api/_lib/puppeteer.ts
@@ -18,7 +18,7 @@ export async function getScreenshot(url, width, height) {
     const page = await getPage();
     await page.goto(url);
     await page.setViewport({ width: Number(width) || 1280, height: Number(height) || 720 });
-    await page.waitForTimeout(3000)
+    await page.waitFor(4000);
     const file = await page.screenshot();
     return file;
 }
