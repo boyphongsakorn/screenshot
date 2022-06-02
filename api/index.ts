b0cf97bfda8173c303e0c1829fd823d2f27f08c5
Phongsakorn Wisetthon <boy1556@hotmail.com>
2022-06-02 13:44:55 +0700

    test

--- api/index.ts
+++ api/index.ts
@@ -3,14 +3,16 @@ import { getScreenshot } from './_lib/puppeteer';
 
 module.exports = async (req: Request, res: Response) => {
   const usage = "https://s.vercel.app/api?url=https://google.com&width=1280&height=720 or https://s.vercel.app/api?lotimg=true&date=01062565"
-  if (!req.query.url && !req.query.lotimg) return res.status(400).json({
-    "success": false,
-    "error": "No url query specified.",
-    "usage": usage
-  });
+  if (!req.query.url) {
+    return res.status(400).json({
+      "success": false,
+      "error": "No url query specified.",
+      "usage": usage
+    });
+  }
   let havelot = false;
   let date = null;
-  if(req.query.lotimg === "true") {
+  if (req.query.lotimg === "true") {
     havelot = true
     date = req.query.date
   };
