5c82560c684e9b127f4987ad334c69ecea44b153
Phongsakorn Wisetthon <boy1556@hotmail.com>
2022-06-02 12:41:54 +0700

    test

--- api/_lib/puppeteer.ts
+++ api/_lib/puppeteer.ts
@@ -1,23 +1,58 @@
-import { launch, Page } from 'puppeteer-core';
-import chrome from 'chrome-aws-lambda';
+import { launch, Page } from "puppeteer-core";
+import chrome from "chrome-aws-lambda";
+import fetch from 'node-fetch';
 let _page: Page | null;
 
 async function getPage() {
     if (_page) return _page;
-    const options = { 
+    const options = {
         args: chrome.args,
         executablePath: await chrome.executablePath,
-        headless: chrome.headless
+        headless: chrome.headless,
     };
     const browser = await launch(options);
     _page = await browser.newPage();
     return _page;
 }
 
-export async function getScreenshot(url, width, height) {
+export async function getScreenshot(url, width, height, havelot, date) {
     const page = await getPage();
-    await page.goto(url);
-    await page.setViewport({ width: Number(width) || 1280, height: Number(height) || 720 });
+    if (havelot) {
+        let datecheck
+        if (date) {
+            datecheck = date;
+        } else {
+            let date = new Date().getDate();
+            let month = new Date().getMonth() + 1;
+            let year = new Date().getFullYear();
+            let byear = year + 543;
+            datecheck = date + '' + month + '' + byear;
+        }
+        const response = await fetch('https://lotapi.pwisetthon.com/?date=' + datecheck);
+        const test = await response.json();
+        let monthtext;
+        switch (datecheck.substring(2, 4)) {
+            case '01': monthtext = "มกราคม"; break;
+            case '02': monthtext = "กุมภาพันธ์"; break;
+            case '03': monthtext = "มีนาคม"; break;
+            case '04': monthtext = "เมษายน"; break;
+            case '05': monthtext = "พฤษภาคม"; break;
+            case '06': monthtext = "มิถุนายน"; break;
+            case '07': monthtext = "กรกฎาคม"; break;
+            case '08': monthtext = "สิงหาคม"; break;
+            case '09': monthtext = "กันยายน"; break;
+            case '10': monthtext = "ตุลาคม"; break;
+            case '11': monthtext = "พฤศจิกายน"; break;
+            case '12': monthtext = "ธันวาคม"; break;
+        }
+        await page.setContent('<head><meta charset="UTF-8"><meta name="viewport" content="width=device-width, initial-scale=1.0"><style>body{font-family: \'Mitr\', font-noto-thai;background-image: url(\'https://lotimg.pwisetthon.com/fbbg\');color: white;}</style></head><h1 style="margin-top: 135px;margin-left: 180px;font-size: 80px;margin-bottom: 0px;">ผลรางวัลสลากกินแบ่งรัฐบาล</h1><h2 style="font-size: 50px;margin-right: 590px;text-align: right;margin-top: -10px;margin-bottom: 0px;">เมื่อประจำวันที่ ' + parseInt(datecheck.substring(0, 2)) + ' ' + monthtext + ' ' + datecheck.substring(4, 8) + '</h2><h2 style="font-size: 80px;margin-left: 450px;margin-top: 25px;margin-bottom: 0px;">รางวัลที่ 1</h2><h2 style="font-size: 11.25vw;margin-left: 190px;margin-top: -65px;margin-right: 650px;text-align: center;margin-bottom: 0px;">' + test[0][1] + '</h2><h2 style="margin-left: 1095px;margin-top: -285px;font-size: 50px;margin-bottom: 15px;">เลขท้าย สองตัว</h2><h2 style="margin-left: 1120px;font-size: 150px;margin-top: -45px;margin-bottom: 0px;">' + test[3][1] + '</h2><h2 style="margin-top: -20px;margin-left: 325px;font-size: 60px;margin-bottom: 0px;">เลขหน้า สามตัว</h2><h2 style="font-size: 5.7vw;margin-left: 260px;margin-top: -15px;">' + test[1][1] + ' | ' + test[1][2] + '</h2><h2 style="margin-left: 875px;margin-top: -300px;font-size: 60px;margin-bottom: 0px;">เลขท้าย สามตัว</h2><h2 style="font-size: 5.7vw;margin-left: 805px;max-width: 475px;margin-top: -15px;">' + test[2][1] + ' | ' + test[2][2] + '</h2>');
+    } else {
+        await page.goto(url);
+    }
+    await page.setViewport({
+        width: Number(width) || 1600,
+        height: Number(height) || 1066,
+    });
     await page.waitFor(5000);
     const file = await page.screenshot();
     return file;
--- api/index.ts
+++ api/index.ts
@@ -2,14 +2,20 @@ import { Request, Response } from '@vercel/node'
 import { getScreenshot } from './_lib/puppeteer';
 
 module.exports = async (req: Request, res: Response) => {
-  const usage = "https://s.vercel.app/api?url=https://google.com&width=1280&height=720"
-  if (!req.query.url) return res.status(400).json({
+  const usage = "https://s.vercel.app/api?url=https://google.com&width=1280&height=720 or https://s.vercel.app/api?lotimg=true&date=01062565"
+  if (!req.query.url || (!req.query.lotimg && !req.query.date)) return res.status(400).json({
     "success": false,
     "error": "No url query specified.",
     "usage": usage
   });
+  let havelot = false;
+  let date = null;
+  if(req.query.lotimg === "true") {
+    havelot = true
+    date = req.query.date
+  };
   try {
-    const file = await getScreenshot(req.query.url, req.query.width, req.query.height);
+    const file = await getScreenshot(req.query.url, req.query.width, req.query.height, havelot, date);
     res.setHeader('Content-Type', `image/png`);
     res.setHeader('Cache-Control', 'no-cache');
     res.status(200).end(file);
